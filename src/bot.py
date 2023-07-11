e9df7ab5846c641dde50f4cf473dc3a0131d35d8
tekofx <tekofxx@gmail.com>
2023-07-11 12:10:00 +0200

    Implementation

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,20 @@
+from Unsplash import Unsplash
+from mastodon import Mastodon
+import asyncio
+import os
+
 class Bot:
     def __init__(self) -> None:
-        pass
+        self.unsplash = Unsplash()
+        self.mastodon=Mastodon(access_token=os.getenv("MASTODON_TOKEN"),api_base_url=os.getenv("MASTODON_APP_INSTANCE"))
+        
+        
+    def run(self):
+        asyncio.run(self.post())
+            
+    async def post(self):
+        
+        pic=self.unsplash.search_keyword("turtle")
+        
+        self.mastodon.status_post("Hello World!")
+        await asyncio.wait(3600) # 1 hour
