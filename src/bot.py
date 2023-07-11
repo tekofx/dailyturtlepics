aa208d8ae65c92398cb4a03154d75c00bdf740a8
tekofx <tekofxx@gmail.com>
2023-07-11 12:18:17 +0200

    Removed twitter

--- src/bot.py
+++ src/bot.py
@@ -2,19 +2,25 @@ from Unsplash import Unsplash
 from mastodon import Mastodon
 import asyncio
 import os
-
+import tweepy as tw
 class Bot:
     def __init__(self) -> None:
         self.unsplash = Unsplash()
         self.mastodon=Mastodon(access_token=os.getenv("MASTODON_TOKEN"),api_base_url=os.getenv("MASTODON_APP_INSTANCE"))
+        
+        """ oauth = tw.OAuthHandler(os.getenv("TWITTER_CONSUMER_KEY"), os.getenv("TWITTER_CONSUMER_SECRET"))
+        oauth.set_access_token(os.getenv("TWITTER_ACCESS_TOKEN"), os.getenv("TWITTER_ACCESS_TOKEN_SECRET"))
+        self.twitter = tw.API(oauth, wait_on_rate_limit=True) """
         
         
     def run(self):
         asyncio.run(self.post())
             
     async def post(self):
+        while True:
         
-        pic=self.unsplash.search_keyword("turtle")
-        
-        self.mastodon.status_post("Hello World!")
-        await asyncio.wait(3600) # 1 hour
+            pic=self.unsplash.search_keyword("turtle")
+            
+            self.mastodon.media_post(pic)
+            #self.twitter.update_status_with_media("Hello World!",pic)
+            await asyncio.wait(3600) # 1 hour
